499eab0e09697a35edca781ae7ae330a419676ce
dukgeunchoi <144686420+dukgeunchoi@users.noreply.github.com>
2026-06-30 20:07:36 +1000

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i, n in enumerate(nums):
+            comp = target - n
+            if comp not in seen:
+                seen[n] = i
+                continue
+            return [seen[comp], i]
+        
